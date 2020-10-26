4447370fe92de5b8329e34c633bf7f570e713691
Ilham Rahman <ilhaamrahman@gmail.com>
2020-10-26 12:03:34 +0700

    implement formik for sign in page

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,35 +1,39 @@
 import { TextField, InputAdornment, Button, Paper } from "@material-ui/core";
 import { LockOutlined, EmailOutlined } from "@material-ui/icons";
-import React, { useState } from "react";
+import React from "react";
 import background from "../assets/wave.svg";
 import image from "../assets/Startup.svg";
 import firebase from "../firebase/firebase";
 import { Link, useHistory } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const CustomInput = ({ field, form, ...props }) => {
+  return (
+    <TextField
+      {...field}
+      {...props}
+      required
+      variant="outlined"
+      fullWidth
+      margin="normal"
+    />
+  );
+};
 
 const SignIn = () => {
   const history = useHistory();
-  const [values, setValues] = useState({
-    email: "",
-    password: "",
-  });
 
-  const handleChange = (e) => {
-    e.persist();
-    setValues((prevValues) => ({
-      ...prevValues,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleSignIn = async (e) => {
+  const handleSignIn = async (values, setSubmitting, resetForm) => {
     const { email, password } = values;
-    e.preventDefault();
     try {
       await firebase.signIn(email, password);
       history.push("/");
     } catch (err) {
       console.error("Auth error", err);
       alert(err.message);
+      setSubmitting(false);
+      resetForm();
     }
   };
 
@@ -54,64 +58,82 @@ const SignIn = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <form noValidate onSubmit={handleSignIn}>
-        <h2 style={{ margin: 0, marginBottom: "8px" }}>Sign In</h2>
-        <img src={image} alt="pic" width="100%" height="180px" />
-        <TextField
-          required
-          type="email"
-          name="email"
-          value={values.email}
-          onChange={handleChange}
-          label="Email"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <EmailOutlined />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <TextField
-          required
-          type="password"
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-          label="Password"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockOutlined />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Button type="submit" variant="contained" color="primary" fullWidth>
-          Sign In
-        </Button>
-        <p style={{ margin: "4px", fontSize: "12px" }}>OR</p>
-        <Button
-          variant="contained"
-          color="secondary"
-          startIcon={<i className="fab fa-google"></i>}
-          style={{ backgroundColor: "#e34134" }}
-          fullWidth
-          onClick={handleSignInWithGoogle}
-        >
-          Sign in with Google
-        </Button>
-        <p style={{ margin: "8px 0 0 0", fontSize: "14px" }}>Forgot password</p>
-        <p style={{ margin: 0, fontSize: "14px" }}>
-          Don't have an account? <Link to="/signup">Sign Up</Link>
-        </p>
-      </form>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Required"),
+          password: Yup.string()
+            .min(6, "Must be 6 characters or more")
+            .required("Required"),
+        })}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          handleSignIn(values, setSubmitting, resetForm);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form noValidate>
+            <h2 style={{ margin: 0, marginBottom: "8px" }}>Sign In</h2>
+            <img src={image} alt="pic" width="100%" height="180px" />
+
+            <Field
+              type="email"
+              name="email"
+              label="Email"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <EmailOutlined />
+                  </InputAdornment>
+                ),
+              }}
+              component={CustomInput}
+            />
+            <ErrorMessage name="email" component="p" className="alert" />
+            <Field
+              type="password"
+              name="password"
+              label="Password"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockOutlined />
+                  </InputAdornment>
+                ),
+              }}
+              component={CustomInput}
+            />
+            <ErrorMessage name="password" component="p" className="alert" />
+            <Button
+              disabled={isSubmitting}
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Sign In
+            </Button>
+            <p style={{ margin: "4px", fontSize: "12px" }}>OR</p>
+            <Button
+              variant="contained"
+              color="secondary"
+              startIcon={<i className="fab fa-google"></i>}
+              style={{ backgroundColor: "#e34134" }}
+              fullWidth
+              onClick={handleSignInWithGoogle}
+            >
+              Sign in with Google
+            </Button>
+            <p style={{ margin: "8px 0 0 0", fontSize: "14px" }}>
+              Forgot password
+            </p>
+            <p style={{ margin: 0, fontSize: "14px" }}>
+              Don't have an account? <Link to="/signup">Sign Up</Link>
+            </p>
+          </Form>
+        )}
+      </Formik>
     </Paper>
   );
 };
